38f2e3c3a4dcc7d6d51318a4c272491f2b750ecc
SAMIR DIAZ <samirdejesus@gmail.com>
2021-10-13 19:19:57 -0500

    mongoose ok

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ import morgan from 'morgan';
 import cors from 'cors';
 import path, { join } from 'path'
 
+// Conexión base de datos
+const mongoose = require('mongoose');
+const uri = 'mongodb://localhost:27017/myapp';
+const options = {useNewUrlParser: true, useUnifiedTopology: true};
+// Or using promises
+mongoose.connect(uri, options).then(
+/** ready to use. The `mongoose.connect()` promise resolves to
+mongoose instance. */
+() => { console.log('Conectado a DB') },
+/** handle initial connection error */
+err => { console.log(err) }
+);
+
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(morgan('tiny'));
-app.get('/', function(req,res){
-    res.send('hello samir de j ');
-});
+
+
+
+app.use('/api', require('./routes/nota'));
 const history = require('connect-history-api-fallback');
 app.use(history());
 app.use(express.static(path.join(__dirname,'public')));
